348f8891f0dbda8f9daaa668b6d97bc9f54ede44
Jim Eckerlein <jim.eckerlein@icloud.com>
2024-11-22 16:27:59 +0100

    branchless design

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use std::arch::asm;
 
 #[cfg(target_arch = "aarch64")]
@@ -10,27 +12,30 @@ fn pi(n: u64) -> f64 {
             "cmp {i}, {n}",
             "bge 4f",
 
-            // Compute 2i + 1
+            // Compute 2i + 1:
+            // - Shift i left by 1 to multiply by 2
             "lsl {term_int}, {i}, 1",
+            // - Add 1 to the result
             "add {term_int}, {term_int}, 1",
+            // - Convert the result to a floating-point number
             "scvtf {term:d}, {term_int}",
 
             // Compute 4.0 / (2i + 1)
             "fdiv {term:d}, {four:d}, {term:d}",
 
-            // Extract LSB of i
+            // Alternate between adding and subtracting the term
+            // - Extract LSB of i
             "and {lsb}, {i}, 1",
+            // - Compare LSB to zero
+            "cmp {lsb}, 0",
+            // - If LSB is not zero, LSB is set to the sign-bit mask, otherwise it is zero
             "csel {lsb}, {mask}, xzr, ne",
-
-            // Test if i is even or odd
-            "and {lsb}, {i}, 1",
-            "cbnz {lsb}, 2f",
-
+            // - Move LSB to a SIMD register
+            "fmov {tmp:d}, {lsb}",
+            // - Xor the term with the sign-bit mask or zero to flip the sign bit if necessary
+            "eor {term:v}.16b, {term:v}.16b, {tmp:v}.16b",
+            // - Add the possibly negated term to the sum
             "fadd {sum:d}, {sum:d}, {term:d}",
-            "b 3f",
-
-            "2:",
-            "fsub {sum:d}, {sum:d}, {term:d}",
 
             // End of loop, increment i
             "3:",
@@ -45,6 +50,7 @@ fn pi(n: u64) -> f64 {
             lsb = out(reg) _,
             mask = in(reg) 0x8000000000000000_u64,
             sum = inout(vreg) sum,
+            tmp = out(vreg) _,
             four = in(vreg) 4.0,
             term_int = out(reg) _,
             term = out(vreg) _,
